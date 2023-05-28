7279b23f904af467b60cd0729f9810c49bdd63d6
Luis Mullisaca <133934612+lmullisaca@users.noreply.github.com>
2023-05-27 21:42:03 -0500

    Add files via upload

---
+++ Hackaton03/Luis Mullisaca/Ejercicio32.js
@@ -0,0 +1,23 @@
+// Solicitar al usuario ingresar el nombre de las provincias
+let provincia1 = prompt("Ingrese el nombre de la provincia 1:");
+let provincia2 = prompt("Ingrese el nombre de la provincia 2:");
+let provincia3 = prompt("Ingrese el nombre de la provincia 3:");
+
+// Solicitar al usuario ingresar el nombre de las ciudades
+let ciudad1 = prompt("Ingrese el nombre de la ciudad 1:");
+let ciudad2 = prompt("Ingrese el nombre de la ciudad 2:");
+let ciudad3 = prompt("Ingrese el nombre de la ciudad 3:");
+let ciudad4 = prompt("Ingrese el nombre de la ciudad 4:");
+let ciudad5 = prompt("Ingrese el nombre de la ciudad 5:");
+let ciudad6 = prompt("Ingrese el nombre de la ciudad 6:");
+let ciudad7 = prompt("Ingrese el nombre de la ciudad 7:");
+let ciudad8 = prompt("Ingrese el nombre de la ciudad 8:");
+let ciudad9 = prompt("Ingrese el nombre de la ciudad 9:");
+let ciudad10 = prompt("Ingrese el nombre de la ciudad 10:");
+let ciudad11 = prompt("Ingrese el nombre de la ciudad 11:");
+
+// Obtener la quinta ciudad como ciudad con mayor población
+let ciudadAleatoria = ciudad5;
+
+// Mostrar la ciudad con mayor población
+console.log(`La ciudad con mayor población es: ${ciudadAleatoria}`);
