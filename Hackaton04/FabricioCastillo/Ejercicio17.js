d09197e5dacd7b26add51e1cf296ea7b1dde83bb
BlackReaper03 <133177961+BlackReaper03@users.noreply.github.com>
2023-06-03 14:27:45 -0500

    FabricioCastilloHacktone04

---
+++ Hackaton04/FabricioCastillo/Ejercicio17.js
@@ -0,0 +1,14 @@
+function eliminarDuplicados(arr) {
+    let arregloSinDuplicados = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arregloSinDuplicados.indexOf(arr[i]) === -1) {
+        arregloSinDuplicados.push(arr[i]);
+      }
+    }
+    return arregloSinDuplicados;
+  }
+
+  let numeros = [1, 2, 3, 4, 2, 3, 5];
+  let numerosSinDuplicados = eliminarDuplicados(numeros);
+  console.log(numerosSinDuplicados); 
+  
