3a8660cbc1d27c8aee10c918c5713b7ffcee9c7c
Paul Rengifo <paul.rengifo.sandoval@rkdsoftware.onmicrosoft.com>
2023-06-03 21:50:55 -0500

    Hackaton 04 - Paul Rengifo

---
+++ Hackaton04/PaulRengifo/Ejercicio04.js
@@ -0,0 +1,8 @@
+//Desarrollar una función que encuentre el número más frecuente en un arreglo de números.
+
+let arr1 = [1,2,5,3,5,6,5,8,5,9] 
+
+
+let numeroRepetido = arr1.some(function(v,i) {return arr1.indexOf(v,i+1)>-1 })
+
+console.log(numeroRepetido)
