cd102853baaf4013c361dac1c1a37836c8f4ba23
Brandon <alex_valen@outloo.com>
2023-06-08 01:40:48 -0500

    nejorando tarea

--- Hackaton04/BrandonValenzuela/tarea4.js
+++ Hackaton04/BrandonValenzuela/tarea4.js
@@ -3,6 +3,6 @@
 let arr1 = [1,2,5,3,5,6,5,8,5,9] 
 
 
-let numeroRepetido = arr1.some(function(v,i) {return arr1.indexOf(v,i+1)>-1 })
+let numeroRepetido = arr1.filter(function(v,i) {return arr1.indexOf(v,i+1)>-1 });
 
-console.log(numeroRepetido)
+console.log(numeroRepetido[0]);
