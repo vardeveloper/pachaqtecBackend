7f5322f46cdd6dcf1dfb4a3b5112dc3571a0551b
Kevin <SV48915324@idat.edu.pe>
2023-08-12 19:29:48 -0500

    KevinPasacheHackaton14

--- Hackaton14/KevinPasache/app.js
+++ Hackaton14/KevinPasache/app.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-/*
+/* Es lo mismo que arriba
 const server = http.createServer(app);
 const io = socketIO(server);
 */
