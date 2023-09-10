7f8990f201b134747b617fe15274e847b3c11370
Yarko Alvarado <133178018+Mr-Yjass@users.noreply.github.com>
2023-09-09 20:27:08 -0500

    ProyectoFinal-Ecommerce-YarkoAlvarado
    Muchas gracias

---
+++ ProyectoFinal/ProyectoFinal-Ecommerce-YarkoAlvarado--/data/prendas.js
@@ -0,0 +1,68 @@
+let prendasInfo = {
+    polos: [
+        {
+            id: 1,
+            nombre:'poloazul',
+            tallas: {
+                talla1: 'M',
+                talla2: 'L',
+                talla3: 'XL'
+            },
+            precio: '30'
+        },
+        {
+            id: 2,
+            nombre:'poloblanco',
+            tallas: {
+                talla1: 'M',
+                talla2: 'L',
+                talla3: 'XL'
+            },
+            precio: '60'
+        },
+        {
+            id: 3,
+            nombre:'polonegro',
+            tallas: {
+                talla1: 'M',
+                talla2: 'L',
+                talla3: 'XL'
+            },
+            precio: '50'
+        }
+    ],
+    pantalones:[
+        {
+            id: 1,
+            nombre:'pantalonazul',
+            tallas: {
+                talla1: '34',
+                talla2: '36',
+                talla3: '38'
+            },
+            precio: '50'
+        },
+        {
+            id: 2,
+            nombre:'pantalonblanco',
+            tallas: {
+                talla1: '34',
+                talla2: '36',
+                talla3: '38'
+            },
+            precio: '50'
+        },
+        {
+            id: 3,
+            nombre:'pantalonnegro',
+            tallas: {
+                talla1: '34',
+                talla2: '36',
+                talla3: '38'
+            },
+            precio: '50'
+        }
+    ]
+};
+
+module.exports.prendasInfo = prendasInfo;
